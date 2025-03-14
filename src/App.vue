01c36bd0f5d6bc47f355d73ff90ac0164e4f1535
Mecki <96236752+Nimixam@users.noreply.github.com>
2025-03-14 11:37:07 +0100

    fix width app

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,46 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch } from "vue";
 
-import Navbar from './components/Navbar.vue'
-import Hero from './components/Hero.vue'
-import EventsMap from './components/EventsMap.vue'
-import Filter from './components/Filter.vue'
-import About from './components/About.vue'
-import Contact from './components/Contact.vue'
-import Footer from './components/Footer.vue'
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import EventsMap from "./components/EventsMap.vue";
+import Filter from "./components/Filter.vue";
+import About from "./components/About.vue";
+import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
 
 // Dark Mode
-const darkMode = ref(false)
+const darkMode = ref(false);
 const toggleDarkMode = () => {
-  darkMode.value = !darkMode.value
-  document.documentElement.classList.toggle('dark', darkMode.value)
-}
+  darkMode.value = !darkMode.value;
+  document.documentElement.classList.toggle("dark", darkMode.value);
+};
 
 // Event-Daten
-const allEvents = ref([])
+const allEvents = ref([]);
 
 // Verwende das Composable für die zentrale Filterlogik
-const filteredEvents = ref([])
+const filteredEvents = ref([]);
 
-const filteredTicketmasterEvents = ref([])
+const filteredTicketmasterEvents = ref([]);
 
-const selectedCategory = ref(''); // Aktuell ausgewählte Kategorie
+const selectedCategory = ref(""); // Aktuell ausgewählte Kategorie
 
 // Filter-Sidebar
-const isFilterOpen = ref(false)
+const isFilterOpen = ref(false);
 const toggleFilter = () => {
-  isFilterOpen.value = !isFilterOpen.value
-}
+  isFilterOpen.value = !isFilterOpen.value;
+};
 
-const filterCatMessage = ref('') // Nachricht für fehlende Events
-const daysToFilter = ref(14) // Standardwert für den Slider
+const filterCatMessage = ref(""); // Nachricht für fehlende Events
+const daysToFilter = ref(14); // Standardwert für den Slider
 
 const showHeatmap = ref(false);
 const showSights = ref(false); // Neue Variable für Sehenswürdigkeiten
 const showTicketmasterEvents = ref(false); // Neue Variable für Sehenswürdigkeiten
 const showCologneEvents = ref(true);
 
-const searchQuery = ref(''); // Suchbegriff
+const searchQuery = ref(""); // Suchbegriff
 const isSearching = ref(false); // Lade-Status
 let searchTimeout; // Timeout für das Debouncing
 
@@ -49,7 +49,6 @@ const onSearchInput = () => {
   clearTimeout(searchTimeout);
 
   searchTimeout = setTimeout(() => {
-
     loadEvents();
 
     isSearching.value = false;
@@ -76,9 +75,9 @@ function debounceLoadEvents() {
 // Events laden (z.B. nächste 14 Tage)
 async function loadEvents() {
   try {
-
-
-    const cacheKey = `${selectedCategory.value}-${daysToFilter.value}-${searchQuery.value.trim().toLowerCase()}`;
+    const cacheKey = `${selectedCategory.value}-${
+      daysToFilter.value
+    }-${searchQuery.value.trim().toLowerCase()}`;
 
     // Falls Daten bereits im Cache sind, nutze diese:
     if (eventsCache.has(cacheKey)) {
@@ -91,8 +90,7 @@ async function loadEvents() {
 
     if (selectedCategory.value) {
       result = await filterByCategory();
-    }
-    else {
+    } else {
       result = await filterByDate();
       allEvents.value = result;
     }
@@ -106,32 +104,30 @@ async function loadEvents() {
     filteredEvents.value = result;
 
     loadTicketmasterEvents();
-
   } catch (error) {
-    filterCatMessage.value = 'Fehler beim Laden der Events'
-    console.error('Fehler beim Laden der Events:', error)
+    filterCatMessage.value = "Fehler beim Laden der Events";
+    console.error("Fehler beim Laden der Events:", error);
   }
 }
 
 async function loadTicketmasterEvents() {
   try {
     if (ticketmasterCache.length === 0) {
-      const response = await fetch('http://localhost:3000/ticketmaster');
+      const response = await fetch("http://localhost:3000/ticketmaster");
       const data = await response.json();
 
-    // Prüfe, ob Events vorhanden sind
-    if (
-      data &&
-      data._embedded &&
-      data._embedded.events &&
-      Array.isArray(data._embedded.events)
-    ) {
-      ticketmasterCache = data._embedded.events;
+      // Prüfe, ob Events vorhanden sind
+      if (
+        data &&
+        data._embedded &&
+        data._embedded.events &&
+        Array.isArray(data._embedded.events)
+      ) {
+        ticketmasterCache = data._embedded.events;
+      } else {
+        console.error("Keine Ticketmaster Events gefunden");
+      }
     }
-    else {
-      console.error('Keine Ticketmaster Events gefunden');
-    }
-  }
 
     let result = ticketmasterCache;
     console.log(result);
@@ -139,30 +135,33 @@ async function loadTicketmasterEvents() {
     // Aktuelles Datum und maxDate berechnen
     const today = new Date();
     const maxDate = new Date();
-    maxDate.setDate(today.getDate() + daysToFilter.value);  // Stichtag berechnen
+    maxDate.setDate(today.getDate() + daysToFilter.value); // Stichtag berechnen
 
     // Filtern der Events
-    result = result.filter(event => {
+    result = result.filter((event) => {
       const eventDate = new Date(event?.dates?.start?.localDate) || maxDate; // Event-Datum umwandeln
-      return eventDate <= maxDate;  // Filtern
+      return eventDate <= maxDate; // Filtern
     });
 
     const query = searchQuery.value.trim().toLowerCase();
     if (query) {
-      result = result.filter(event => event.name.toLowerCase().includes(query));
+      result = result.filter((event) =>
+        event.name.toLowerCase().includes(query)
+      );
     }
     filteredTicketmasterEvents.value = result;
 
     console.log(filteredTicketmasterEvents.value);
-
   } catch (error) {
-    console.error('Fehler beim Laden der Ticketmaster-Events:', error);
+    console.error("Fehler beim Laden der Ticketmaster-Events:", error);
   }
 }
 
 async function filterByDate(days = daysToFilter.value) {
-  const response = await fetch(`http://localhost:3000/api/events?ndays=${days}`)
-  const data = await response.json()
+  const response = await fetch(
+    `http://localhost:3000/api/events?ndays=${days}`
+  );
+  const data = await response.json();
   let valid;
 
   // data => { success: true, items: [...], count: NN }
@@ -183,24 +182,24 @@ async function filterByDate(days = daysToFilter.value) {
       district: item.stadtteil,
       price: item.preis,
       link: item.link,
-      img: item.teaserbild
+      img: item.teaserbild,
     }));
 
     // Filtern wir optional nach gültigen lat/lng
-    valid = mapped.filter(ev => !isNaN(ev.lat) && !isNaN(ev.lng))
-
-
+    valid = mapped.filter((ev) => !isNaN(ev.lat) && !isNaN(ev.lng));
   } else {
-    filterCatMessage.value = 'Es gibt keine Events mit dem Filter'
-    console.error('Unerwartetes Format für Events:', data)
+    filterCatMessage.value = "Es gibt keine Events mit dem Filter";
+    console.error("Unerwartetes Format für Events:", data);
   }
 
   return valid;
 }
 
 async function filterByCategory() {
-  const response = await fetch(`http://localhost:3000/api/eventsByCategory?kat=${selectedCategory.value}&ndays=${daysToFilter.value}`)
-  const data = await response.json()
+  const response = await fetch(
+    `http://localhost:3000/api/eventsByCategory?kat=${selectedCategory.value}&ndays=${daysToFilter.value}`
+  );
+  const data = await response.json();
   let valid;
 
   if (data.success && Array.isArray(data.items)) {
@@ -219,114 +218,174 @@ async function filterByCategory() {
       district: item.stadtteil,
       price: item.preis,
       link: item.link,
-      img: item.teaserbild
-    }))
+      img: item.teaserbild,
+    }));
 
-    valid = mapped.filter(ev => !isNaN(ev.lat) && !isNaN(ev.lng))
-
+    valid = mapped.filter((ev) => !isNaN(ev.lat) && !isNaN(ev.lng));
   } else {
-    filterCatMessage.value = 'Es gibt keine Events mit dem Filter'
-    console.error('Unerwartetes Format für Events:', data)
+    filterCatMessage.value = "Es gibt keine Events mit dem Filter";
+    console.error("Unerwartetes Format für Events:", data);
   }
 
   return valid;
 }
-
 
 function filterBySearchQuery(eventsData) {
   isSearching.value = false;
 
   const query = searchQuery.value.trim().toLowerCase();
 
-  return eventsData.filter(event =>
-  (event.title?.toLowerCase().includes(query) ||
-    event.description?.toLowerCase().includes(query))
+  return eventsData.filter(
+    (event) =>
+      event.title?.toLowerCase().includes(query) ||
+      event.description?.toLowerCase().includes(query)
   );
 }
 
-
 // Lade die Events, sobald App gemountet ist
-loadEvents()
+loadEvents();
 
 // Überwache die Events-Arrays
 watch([allEvents, filteredEvents], ([newAllEvents, newFilteredEvents]) => {
   if (newAllEvents.length === 0 || newFilteredEvents.length === 0) {
-    filterCatMessage.value = 'Es gibt keine Stadt Köln Events mit dem Filter'
+    filterCatMessage.value = "Es gibt keine Stadt Köln Events mit dem Filter";
   } else {
-    filterCatMessage.value = '' // Nachricht zurücksetzen, wenn es Events gibt
+    filterCatMessage.value = ""; // Nachricht zurücksetzen, wenn es Events gibt
   }
-})
+});
 </script>
 
 <template>
-  <div class="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+  <div
+    class="container mx-auto px-4 min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white"
+  >
     <Navbar :darkMode="darkMode" @toggleDarkMode="toggleDarkMode" />
     <Hero />
 
     <!-- Suchleiste, Checkboxes & Filter-Button in einer Zeile -->
-    <div class="flex items-center justify-between mt-4 px-4 space-x-4">
+    <div class="flex items-center justify-between mt-4 space-x-4">
       <!-- Suchleiste -->
       <div class="relative flex-1 min-w-[200px]">
-        <input type="text" v-model="searchQuery" @input="onSearchInput" placeholder="Events durchsuchen"
-          class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" />
+        <input
+          type="text"
+          v-model="searchQuery"
+          @input="onSearchInput"
+          placeholder="Events durchsuchen"
+          class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+        />
         <div v-if="isSearching" class="absolute right-2 top-2">
-          <svg class="animate-spin h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+          <svg
+            class="animate-spin h-5 w-5 text-green-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            ></path>
           </svg>
         </div>
       </div>
 
-
-      <!-- Filter Button & Heatmap Checkbox -->
       <!-- Checkboxes -->
       <div class="flex items-center gap-2">
-        <!-- Stadt Köln Events Checkbox -->
-        <input type="checkbox" id="stadtkoeln-toggle" v-model="showCologneEvents" class="mr-1 cursor-pointer" />
-        <label for="stadtkoeln-toggle" class="text-sm font-medium text-gray-900 dark:text-white">
+        <input
+          type="checkbox"
+          id="stadtkoeln-toggle"
+          v-model="showCologneEvents"
+          class="mr-1 cursor-pointer"
+        />
+        <label for="stadtkoeln-toggle" class="text-sm font-medium">
           Events (Stadt Köln)
         </label>
-        <!-- Ticketmaster Checkbox -->
-        <input type="checkbox" id="ticketmaster-toggle" v-model="showTicketmasterEvents" class="mr-1 cursor-pointer" />
-        <label for="ticketmaster-toggle" class="text-sm font-medium text-gray-900 dark:text-white">
+        <input
+          type="checkbox"
+          id="ticketmaster-toggle"
+          v-model="showTicketmasterEvents"
+          class="mr-1 cursor-pointer"
+        />
+        <label for="ticketmaster-toggle" class="text-sm font-medium">
           Großevents (Ticketmaster)
         </label>
-        <!-- Sehenswürdigkeiten Checkbox -->
-        <input type="checkbox" id="sights-toggle" v-model="showSights" class="mr-1 cursor-pointer" />
-        <label for="sights-toggle" class="text-sm font-medium text-gray-900 dark:text-white">
+        <input
+          type="checkbox"
+          id="sights-toggle"
+          v-model="showSights"
+          class="mr-1 cursor-pointer"
+        />
+        <label for="sights-toggle" class="text-sm font-medium">
           Sehenswürdigkeiten
         </label>
-        <input type="checkbox" id="heatmap-toggle" v-model="showHeatmap" class="mr-1 cursor-pointer" />
-        <label for="heatmap-toggle" class="text-sm font-medium text-gray-900 dark:text-white">
+        <input
+          type="checkbox"
+          id="heatmap-toggle"
+          v-model="showHeatmap"
+          class="mr-1 cursor-pointer"
+        />
+        <label for="heatmap-toggle" class="text-sm font-medium">
           Heatmap
         </label>
       </div>
 
       <!-- Filter-Button -->
-      <button class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 focus:outline-none"
-        @click="toggleFilter">
-        {{ isFilterOpen ? 'Filter ausblenden' : 'Filter einblenden' }}
+      <button
+        class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 focus:outline-none"
+        @click="toggleFilter"
+      >
+        {{ isFilterOpen ? "Filter ausblenden" : "Filter einblenden" }}
       </button>
     </div>
 
     <!-- Map, Liste, Filter -->
-    <div class="relative flex transition-all duration-300 mt-4" style="height: 60vh;">
-      <div class="flex-none transition-all duration-300" :class="isFilterOpen ? 'w-3/4' : 'w-full'">
-        <!-- Übergebe gefilterte Events an die Kartenkomponente -->
-        <EventsMap :filteredEvents="filteredEvents" :filteredTicketmasterEvents="filteredTicketmasterEvents"
-          :showHeatmap="showHeatmap" :showSights="showSights" :showTicketmasterEvents="showTicketmasterEvents"
-          :showCologneEvents="showCologneEvents" />
+    <div
+      class="relative flex transition-all duration-300 mt-4"
+      style="height: 60vh"
+    >
+      <div
+        class="flex-none transition-all duration-300"
+        :class="isFilterOpen ? 'w-3/4' : 'w-full'"
+      >
+        <EventsMap
+          :filteredEvents="filteredEvents"
+          :filteredTicketmasterEvents="filteredTicketmasterEvents"
+          :showHeatmap="showHeatmap"
+          :showSights="showSights"
+          :showTicketmasterEvents="showTicketmasterEvents"
+          :showCologneEvents="showCologneEvents"
+        />
       </div>
 
-      <transition enter-active-class="transition ease-out duration-300" enter-from-class="opacity-0 translate-x-full"
-        enter-to-class="opacity-100 translate-x-0" leave-active-class="transition ease-in duration-200"
-        leave-from-class="opacity-100 translate-x-0" leave-to-class="opacity-0 translate-x-full">
-        <div v-if="isFilterOpen"
-          class="flex-none w-1/4 bg-gray-50 dark:bg-gray-800 text-black dark:text-white p-4 overflow-y-auto shadow-lg">
+      <transition
+        enter-active-class="transition ease-out duration-300"
+        enter-from-class="opacity-0 translate-x-full"
+        enter-to-class="opacity-100 translate-x-0"
+        leave-active-class="transition ease-in duration-200"
+        leave-from-class="opacity-100 translate-x-0"
+        leave-to-class="opacity-0 translate-x-full"
+      >
+        <div
+          v-if="isFilterOpen"
+          class="flex-none w-1/4 bg-gray-50 dark:bg-gray-800 text-black dark:text-white p-4 overflow-y-auto shadow-lg"
+        >
           <h2 class="text-lg font-bold mb-2">Filter</h2>
-          <Filter :allEvents="allEvents" :filterCatMessage="filterCatMessage" v-model:daysToFilter="daysToFilter"
-            v-model:selectedCategory="selectedCategory" @reloadEvents="debounceLoadEvents" />
+          <Filter
+            :allEvents="allEvents"
+            :filterCatMessage="filterCatMessage"
+            v-model:daysToFilter="daysToFilter"
+            v-model:selectedCategory="selectedCategory"
+            @reloadEvents="debounceLoadEvents"
+          />
           <p v-if="filterCatMessage" class="text-red-600 dark:text-red-400">
             {{ filterCatMessage }}
           </p>
