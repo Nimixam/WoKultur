cc2368341037540d2732914eb1d35eae94864c0f
dvowe <david.vowe@gmail.com>
2025-03-14 18:36:19 +0100

    filter einblenden button farbe update

--- src/App.vue
+++ src/App.vue
@@ -157,7 +157,7 @@
 
       <!-- Filter-Button -->
       <button
-        class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 focus:outline-none"
+        class="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700 focus:outline-none"
         @click="toggleFilter"
       >
         {{ isFilterOpen ? "Filter ausblenden" : "Filter einblenden" }}
